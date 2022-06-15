ac100b4fea651738c9682961dc341f9379348ab2
sergeykhach <sosesallc@gmail.com>
2022-06-15 17:52:21 +0400

    2com

---
+++ paha.js
@@ -0,0 +1,14 @@
+import {fileOpen, directoryOpen,fileSave,supported} from 'https://unpkg.com/browser-fs-access';
+
+async function pahatexty() {
+        alert( 'Function is working!' );
+  var userInput = document.getElementById("myText").value;
+  var blob = new Blob([userInput], { type: "text/plain;charset=utf-8" });
+      const fileHandle = await window.showSaveFilePicker();
+      // const fileHandle = await window.chooseFileSystemEntries();
+      const fileStream = await fileHandle.createWritable();
+      await fileStream.write(blob);
+      await fileStream.close();
+}
+
+window.pahatexty=pahatexty;
